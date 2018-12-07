b5a1680c0a34c92fcc42e18f378d835d95108906
Mihai <mihaistancu@mail.com>
2018-12-07 19:30:31 +0200

    Adds canvas and ability to draw lines on it

--- GEOproject/MainWindow.xaml.cs
+++ GEOproject/MainWindow.xaml.cs
@@ -20,9 +20,34 @@ namespace GEOproject
     /// </summary>
     public partial class MainWindow : Window
     {
+        Point prevPoint = new Point();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Canvas_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if ((prevPoint.X == 0) && (prevPoint.Y == 0))
+            {
+                prevPoint.X = e.GetPosition(this).X;
+                prevPoint.Y = e.GetPosition(this).Y;
+            }
+            else
+            {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = prevPoint.X;
+                line.Y1 = prevPoint.Y;
+                line.X2 = e.GetPosition(this).X;
+                line.Y2 = e.GetPosition(this).Y;
+
+                prevPoint = e.GetPosition(this);
+
+                drawSurface.Children.Add(line);
+            }
         }
     }
 }
